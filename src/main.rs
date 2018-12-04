06d1b64a654a0bd563e1572c0189991039f668cc
yuuya uezato <yuuya_uezato@dwango.co.jp>
2018-12-04 15:08:51 +0900

    frugalosの作ったlusfを確認するためのブランチ
    まずPUTやDELETEやJOURNAL_GCなどディスクを間違いなく変更してしまう操作類を削除した

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,8 @@ extern crate rustyline;
 
 use kanils::handle::StorageHandle;
 
-use cannyls::block::BlockSize;
-use cannyls::lump::LumpId;
 use cannyls::nvm::FileNvm;
-use cannyls::storage::{Storage, StorageBuilder};
+use cannyls::storage::StorageBuilder;
 
 use regex::Regex;
 
@@ -27,18 +25,12 @@ use rustyline::Editor;
 
 use std::path::PathBuf;
 use std::str;
-use std::time::SystemTime;
 
 use structopt::StructOpt;
 
 arg_enum! {
     #[derive(Debug)]
     enum Command {
-        // capacityバイトの容量を持つlusfストレージを新たに生成する
-        // kanils Create --storage=storage_path --capacity=num
-        // (storage_pathが既に存在する場合には何もしない)
-        Create,
-
         // lusfストレージ中のデータをダンプする
         // kanils Dump --storage=storage_path
         Dump,
@@ -46,11 +38,6 @@ arg_enum! {
         // lusfストレージ中に存在するlumpid一覧を出力する
         // kanils List --storage=storage_path
         List,
-
-        // lusfストレージに、keyをkey, valueをstringとしてkey-value組を追加する
-        // 既にkeyが存在する場合は上書きする挙動に注意
-        // kanils Put --storage=storage_path --key=lumpid --data=string
-        Put,
 
         // lusfストレージの指定したkeyを持つ値を「文字列として」取得する
         // 存在しないkeyが指定された場合はその旨が出力される
@@ -62,11 +49,6 @@ arg_enum! {
         // kanils Get --storage=storage_path --key=lumpid
         GetBytes,
 
-        // lusfストレージの指定したkeyを削除する
-        // 存在しないkeyが指定された場合はその旨が出力される
-        // kanils Delete --storage=storage_path --key=lumpid
-        Delete,
-
         // lusfストレージ中のヘッダ情報を出力する
         // ヘッダ情報についての詳細は https://github.com/frugalos/cannyls/wiki/Storage-Format を参照
         // kanils Header --storage=storage_path
@@ -76,24 +58,10 @@ arg_enum! {
         // kanils Journal --storage=storage_path
         Journal,
 
-        // lusfストレージ中のジャーナル領域に対してfull GCを行う
-        // kanils JournalGC --storage=storage_path
-        JournalGC,
-
         // 存在するlusfストレージを開き
         // 対話的に Dump, List, Put, Get, Delete, Header の操作を試すことができる
         // kanils Open --storage=storage_path
         Open,
-
-        // 新たにlusfストレージを作成し、1件sizeバイト長データを、count個書き込む
-        // 書き込みのみを行う簡易ベンチマークツール
-        // kanils WBench --stoage=storage_path --count=number --size=number
-        WBench,
-
-        // 新たにlusfストレージを作成し、1件sizeバイト長データを、cout個書き込みつつ
-        // 読み込みも行うような、書き込み読み込み混合の簡易ベンチマークツール
-        // kanils WRBench --storage=storage_path --count=number --size=number
-        WRBench,
     }
 }
 
@@ -144,58 +112,16 @@ fn string_to_u128(lumpid_str: &str) -> u128 {
     }
 }
 
-fn is_valid_characters(data: &str) -> bool {
-    std::str::from_utf8(data.as_bytes()).is_ok()
-}
-
-fn create_storage_for_benchmark(
-    path: PathBuf,
-    count: u64,
-    size: u64,
-) -> Result<(Storage<FileNvm>, u64), cannyls::Error> {
-    let total = count * size;
-    let capacity = total * 2;
-    let mut journal_ratio = 0.01f64;
-    if ((capacity as f64 * journal_ratio) as u64) < 256 * count as u64 {
-        journal_ratio = (256 * count as u64) as f64 / capacity as f64;
-    }
-    let nvm: FileNvm = track_try_unwrap!(FileNvm::create(path, capacity as u64));
-    track!(
-        StorageBuilder::new()
-            .journal_region_ratio(journal_ratio)
-            .create(nvm)
-    ).map(|s| (s, total as u64))
-}
-
 fn handle_input(handle: &mut StorageHandle, input: &str) {
-    let put_regex = Regex::new(r"^put\s+([0-9]+|0x[0-9a-f]+)\s+([^\x00]+)$").unwrap();
     let get_regex = Regex::new(r"^get\s+([0-9]+|0x[0-9a-f]+)$").unwrap();
     let get_as_bytes_regex = Regex::new(r"^get_bytes\s+([0-9]+|0x[0-9a-f]+)$").unwrap();
-    let delete_regex = Regex::new(r"^delete\s*([0-9]+|0x[0-9a-f]+)$").unwrap();
 
-    if let Some(captured) = put_regex.captures(&input) {
-        println!("captured = {:?}", captured);
-
-        let key: u128 = string_to_u128(captured.get(1).unwrap().as_str());
-        let value: &str = captured.get(2).unwrap().as_str();
-
-        println!("key = {:?}", key);
-        println!("value = {:?}", value);
-
-        if is_valid_characters(value) {
-            handle.put(key, value);
-        } else {
-            println!("your input value {} is invalid wrt UTF-8", input);
-        }
-    } else if let Some(captured) = get_regex.captures(&input) {
+    if let Some(captured) = get_regex.captures(&input) {
         let key: u128 = string_to_u128(captured.get(1).unwrap().as_str());
         handle.get(key);
     } else if let Some(captured) = get_as_bytes_regex.captures(&input) {
         let key: u128 = string_to_u128(captured.get(1).unwrap().as_str());
         handle.print_as_bytes(key);
-    } else if let Some(captured) = delete_regex.captures(&input) {
-        let key: u128 = string_to_u128(captured.get(1).unwrap().as_str());
-        handle.delete(key);
     } else if input == "list" {
         handle.print_list_of_lumpids();
     } else if input == "dump" {
@@ -204,8 +130,6 @@ fn handle_input(handle: &mut StorageHandle, input: &str) {
         handle.print_header_info();
     } else if input == "journal" {
         handle.print_journal_info();
-    } else if input == "journal_gc" {
-        handle.journal_gc();
     } else {
         println!("`{}` is an invalid command", input);
     }
@@ -215,45 +139,6 @@ fn main() {
     let opt = Opt::from_args();
 
     match opt.command {
-        Command::Create => {
-            let mut data_region_size = opt.capacity.unwrap();
-            println!("passed data region size = {}", data_region_size);
-            let block_size = BlockSize::min();
-            let block_size_u64 = u64::from(block_size.as_u16());
-
-            data_region_size = block_size.ceil_align(data_region_size);
-
-            let journal_header_size = block_size_u64;
-            let journal_record_size =
-                std::cmp::max(block_size_u64 * 2, 20 * (data_region_size / block_size_u64));
-            let journal_region_size = journal_header_size + journal_record_size;
-
-            let header_size = block_size_u64;
-
-            let total_size = data_region_size + journal_region_size + header_size;
-            let journal_ratio: f64 = 0.01f64.max(journal_region_size as f64 / total_size as f64);
-
-            let nvm = track_try_unwrap!(FileNvm::create(opt.storage_path, total_size));
-            let storage = track_try_unwrap!(
-                StorageBuilder::new()
-                    .journal_region_ratio(journal_ratio)
-                    .create(nvm)
-            );
-
-            println!("---------------");
-            let actual_data_region_size = storage.header().data_region_size;
-            let actual_journal_region_size = storage.header().journal_region_size;
-            println!("actual data region size = {}", actual_data_region_size);
-            println!(
-                "actual journal region size = {}",
-                actual_journal_region_size
-            );
-            println!(
-                "actual journal region size ratio = {}",
-                (actual_journal_region_size as f64)
-                    / (actual_journal_region_size + actual_data_region_size) as f64
-            );
-        }
         Command::Open => {
             let nvm = track_try_unwrap!(FileNvm::open(&opt.storage_path));
             let storage = track_try_unwrap!(StorageBuilder::new().open(nvm));
@@ -292,27 +177,13 @@ fn main() {
             let lumpid_str: String = opt.lumpid.unwrap();
             handle.print_as_bytes(string_to_u128(&lumpid_str));
         }
-        Command::Put => {
-            let mut handle = StorageHandle::create(&opt.storage_path);
-            let lumpid_str: String = opt.lumpid.unwrap();
-            handle.put(string_to_u128(&lumpid_str), &opt.data.unwrap());
-        }
         Command::Journal => {
             let mut handle = StorageHandle::create(&opt.storage_path);
             handle.print_journal_info();
         }
-        Command::JournalGC => {
-            let mut handle = StorageHandle::create(&opt.storage_path);
-            handle.journal_gc();
-        }
         Command::List => {
             let mut handle = StorageHandle::create(&opt.storage_path);
             handle.print_list_of_lumpids();
-        }
-        Command::Delete => {
-            let mut handle = StorageHandle::create(&opt.storage_path);
-            let lumpid_str: String = opt.lumpid.unwrap();
-            handle.delete(string_to_u128(&lumpid_str));
         }
         Command::Dump => {
             let mut handle = StorageHandle::create(&opt.storage_path);
@@ -321,62 +192,6 @@ fn main() {
         Command::Header => {
             let mut handle = StorageHandle::create(&opt.storage_path);
             handle.print_header_info();
-        }
-        Command::WBench => {
-            let count = opt.count.unwrap();
-            let size = opt.size.unwrap();
-            let (mut storage, total) =
-                create_storage_for_benchmark(opt.storage_path, count as u64, size as u64).unwrap();
-            let tmp_vec: Vec<u8> = vec![0; size];
-
-            let now = SystemTime::now();
-
-            for i in 0..count {
-                let lump_id = LumpId::new(i);
-                let lump_data =
-                    track_try_unwrap!(storage.allocate_lump_data_with_bytes(tmp_vec.as_ref()));
-                storage.put(&lump_id, &lump_data).unwrap();
-                storage.journal_sync().unwrap();
-            }
-
-            if let Ok(elapsed) = now.elapsed() {
-                println!("total = {}Byte, elapsed = {:?}", total, elapsed);
-            }
-        }
-        Command::WRBench => {
-            let count = opt.count.unwrap();
-            let size = opt.size.unwrap();
-            let (mut storage, total) =
-                create_storage_for_benchmark(opt.storage_path, count as u64, size as u64).unwrap();
-            let tmp_vec: Vec<u8> = vec![0; size];
-
-            let now = SystemTime::now();
-
-            // access pattern: marching
-            let marching_len = 100;
-            let mut c = 0;
-            let mut keystore = Vec::with_capacity(marching_len);
-            for i in 0..count {
-                let lump_id = LumpId::new(i);
-                let lump_data =
-                    track_try_unwrap!(storage.allocate_lump_data_with_bytes(tmp_vec.as_ref()));
-                storage.put(&lump_id, &lump_data).unwrap();
-                if c < marching_len - 1 {
-                    keystore.push(lump_id);
-                    c += 1;
-                } else {
-                    // c == marching_len - 1
-                    for k in &keystore {
-                        let _ = storage.get(k);
-                    }
-                    keystore.clear();
-                    c = 0;
-                }
-            }
-
-            if let Ok(elapsed) = now.elapsed() {
-                println!("total = {}Byte, elapsed = {:?}", total, elapsed);
-            }
         }
     }
 }
